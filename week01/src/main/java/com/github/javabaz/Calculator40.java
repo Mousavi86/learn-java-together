1dfe1455fc61ba9a66bc79e1cc0ff9976466d99f
Mousavi86 <mousavimaryam86@yahoo.com>
2025-03-27 22:18:12 +0330

    cal40

---
+++ week01/src/main/java/com/github/javabaz/Calculator40.java
@@ -0,0 +1,49 @@
+package com.github.javabaz;
+
+import java.util.Scanner;
+
+public class Calculator40 {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
+//public void main(String[] args) {
+//    Scanner s = new Scanner(System.in);
+//    Calculator40 c;
+//    c = new Calculator40();
+//    try {
+//        System.out.println("Enter first number: ");
+//        int fNumber = s.nextInt();
+//        System.out.println("Enter second number");
+//        int sNumber = s.nextInt();
+//        System.out.println("Enter operator:");
+//        char operator = s.next().charAt(0);
+//        int result = 0;
+//        if (operator == '+') {
+//            result = c.add(fNumber, sNumber);
+//        } else if (operator == '-') {
+//            result = c.subtract(fNumber, sNumber);
+//        } else if (operator == '*') {
+//            result = c.multiply(fNumber, sNumber);
+//        } else if (operator == '/') {
+//            result = c.divide(fNumber, sNumber);
+//        } else {
+//            System.out.println("Invalid operator!");
+//        }
+//        System.out.println("result: " + result);
+//    } catch (Exception e) }
+
+
